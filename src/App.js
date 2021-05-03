2a71522396d4666b46c271fdc062888718d43e58
Salem <nlsabm@yahoo.com>
2021-05-03 03:50:37 -0400

    search github users

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Layout/Navbar';
 import Users from './Components/Users/Users';
 import Search from './Components/Users/Search'
+import Alert from './Components/Layout/Alert'
+import About from './Components/Pages/About'
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
-  // async and await
-  async componentDidMount(){
-    this.setState({loading: true});
+  // search github users
+    searchUsers = async text => {
+      // loading spinner to true
+      this.setState({loading: true});
+      // searching for github users
+      const res = await axios.get(`https://api.github.com/search/users?q=${text}`, {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+        }
+      });
+      this.setState({loading: false, users: res.data.items});
+    }
+    // clearing the users in the clear btn
+    clearUsers = () => this.setState({users: [], loading: false});
 
-    const res = await axios.get(`https://api.github.com/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-      }
-    });
-
-    this.setState({users: res.data, loading: false});
-  }
-
+    // setAlert to give an alert whne user submits  an empty str
+    setAlert = (msg, type) => {
+      this.setState({ alert: {msg, type} })
+      setTimeout(() => this.setState({alert: null}), 4000)
+    }
   render(){
+    const {users, loading} = this.state
    return (
+     <Router>
      <div className="App">
        <Navbar />
        <div className="container">
-         <Search />
-       <Users loading= {this.state.loading} users= {this.state.users} />
+         <Alert alert={this.state.alert} />
+         <Switch>
+           <Route exact path='/' render={props => (
+             <Fragment>
+               <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} setAlert={this.setAlert} />
+       <Users loading= {loading} users= {users} />
+             </Fragment>
+           )} />
+           <Route exact path='/about' component={About} />
+         </Switch>
+         
        </div>
      </div>
+     </Router>
    );
  }
 }
-
 export default App;
--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
   state = {
@@ -6,17 +7,34 @@ export class Search extends Component {
   }
 
   onChange = (e) => this.setState({[e.target.name]: e.target.value})
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+  } 
+
   onSubmit = e => {
-  console.log(this.state.text)
+    if(this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light')
+    } else {
+      this.props.searchUsers(this.state.text)
+      this.setState({ text: '' });
+    }
   e.preventDefault();
   }
   render() {
+    const {showClear, clearUsers} = this.props
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
           <input type="text" name="text" placeholder='Search Users...' value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value="Search" className='btn btn-dark btn-block' />
         </form>
+        {showClear && (<button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>)}
+        
       </div>
     )
   }
